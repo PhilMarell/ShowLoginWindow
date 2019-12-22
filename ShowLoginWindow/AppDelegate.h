02170743b00e479f8b532eb218728c892c345c7a
Phil Marell <phil@philmarell.com>
2019-12-22 15:27:10 +1100

    Initial Commit

---
+++ ShowLoginWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShowLoginWindow
+//
+//  Created by Phil Marell on 22/12/19.
+//  Copyright © 2019 Phil Marell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ShowLoginWindow/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ShowLoginWindow
+//
+//  Created by Phil Marell on 22/12/19.
+//  Copyright © 2019 Phil Marell. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
